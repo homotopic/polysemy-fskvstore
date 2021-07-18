b4982fdabc291d95e39fd3fa916cf451439fbae8
Daniel Firth <dan.firth@homotopic.tech>
2021-07-18 12:03:25 +0200

    Fix version bounds, inlining and docs.

--- src/Polysemy/FSKVStore.hs
+++ src/Polysemy/FSKVStore.hs
@@ -1,34 +1,39 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE LambdaCase     #-}
-{-# LANGUAGE PolyKinds      #-}
-{-# LANGUAGE TypeOperators  #-}
-module Polysemy.FSKVStore (
-  FSKVStore
-, runFSKVStoreRelBS
-, runFSKVStoreAbsBS
-, runFSKVStoreRelUtf8
-, runFSKVStoreAbsUtf8
-) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeOperators #-}
 
-import           Data.ByteString         as BS
-import           Path
-import           Polysemy
-import           Polysemy.KVStore
-import           RIO                     (Text, readFileUtf8, writeFileUtf8)
+module Polysemy.FSKVStore
+  ( FSKVStore,
+    runFSKVStoreRelBS,
+    runFSKVStoreAbsBS,
+    runFSKVStoreRelUtf8,
+    runFSKVStoreAbsUtf8,
+  )
+where
+
+import Data.ByteString as BS
+import Path
+import Polysemy
+import Polysemy.KVStore
+import RIO (Text, readFileUtf8, writeFileUtf8)
 import qualified UnliftIO.Path.Directory as U
 
 -- | Type synonym for a KVStore indexed by files.
 type FSKVStore b a = KVStore (Path b File) a
 
 -- | Run an `FSKVStore Rel ByteString` in the supplied directory in IO.
-runFSKVStoreRelBS :: Members '[Embed IO] r
-                  => Path b Dir
-                  -> Sem (FSKVStore Rel ByteString ': r) a
-                  -> Sem r a
+--
+-- @since 0.1.0.0
+runFSKVStoreRelBS ::
+  Members '[Embed IO] r =>
+  Path b Dir ->
+  Sem (FSKVStore Rel ByteString ': r) a ->
+  Sem r a
 runFSKVStoreRelBS d = interpret \case
-  LookupKV k   -> embed $ do
+  LookupKV k -> embed $ do
     z <- U.doesFileExist (d </> k)
     if z
       then fmap Just . BS.readFile $ toFilePath $ d </> k
@@ -37,14 +42,18 @@ runFSKVStoreRelBS d = interpret \case
     U.createDirectoryIfMissing True (d </> parent k)
     case v of
       Nothing -> pure ()
-      Just x  -> BS.writeFile (toFilePath (d </> k)) x
+      Just x -> BS.writeFile (toFilePath (d </> k)) x
+{-# INLINE runFSKVStoreRelBS #-}
 
 -- | Run an `FSKVStore Abs ByteString` in IO.
-runFSKVStoreAbsBS :: Members '[Embed IO] r
-                  => Sem (FSKVStore Abs ByteString ': r) a
-                  -> Sem r a
+--
+-- @since 0.1.0.0
+runFSKVStoreAbsBS ::
+  Members '[Embed IO] r =>
+  Sem (FSKVStore Abs ByteString ': r) a ->
+  Sem r a
 runFSKVStoreAbsBS = interpret \case
-  LookupKV k   -> embed $ do
+  LookupKV k -> embed $ do
     z <- U.doesFileExist k
     if z
       then fmap Just . BS.readFile $ toFilePath k
@@ -53,15 +62,19 @@ runFSKVStoreAbsBS = interpret \case
     U.createDirectoryIfMissing True (parent k)
     case v of
       Nothing -> pure ()
-      Just x  -> BS.writeFile (toFilePath k) x
+      Just x -> BS.writeFile (toFilePath k) x
+{-# INLINE runFSKVStoreAbsBS #-}
 
 -- | Run an `FSKVStore Rel Text` in the supplied directory in IO as UTF8.
-runFSKVStoreRelUtf8 :: Members '[Embed IO] r
-                    => Path b Dir
-                    -> Sem (FSKVStore Rel Text ': r) a
-                    -> Sem r a
+--
+-- @since 0.1.0.0
+runFSKVStoreRelUtf8 ::
+  Members '[Embed IO] r =>
+  Path b Dir ->
+  Sem (FSKVStore Rel Text ': r) a ->
+  Sem r a
 runFSKVStoreRelUtf8 d = interpret \case
-  LookupKV k   -> do
+  LookupKV k -> do
     z <- U.doesFileExist (d </> k)
     if z
       then fmap Just . readFileUtf8 $ toFilePath $ d </> k
@@ -70,14 +83,18 @@ runFSKVStoreRelUtf8 d = interpret \case
     U.createDirectoryIfMissing True (d </> parent k)
     case v of
       Nothing -> pure ()
-      Just x  -> writeFileUtf8 (toFilePath (d </> k)) x
+      Just x -> writeFileUtf8 (toFilePath (d </> k)) x
+{-# INLINE runFSKVStoreRelUtf8 #-}
 
 -- | Run an `FSKVStore Abs Text` in IO as UTF8.
-runFSKVStoreAbsUtf8 :: Members '[Embed IO] r
-                    => Sem (FSKVStore Abs Text ': r) a
-                    -> Sem r a
+--
+-- @since 0.1.0.0
+runFSKVStoreAbsUtf8 ::
+  Members '[Embed IO] r =>
+  Sem (FSKVStore Abs Text ': r) a ->
+  Sem r a
 runFSKVStoreAbsUtf8 = interpret \case
-  LookupKV k   -> do
+  LookupKV k -> do
     z <- U.doesFileExist k
     if z
       then fmap Just . readFileUtf8 $ toFilePath k
@@ -86,4 +103,5 @@ runFSKVStoreAbsUtf8 = interpret \case
     U.createDirectoryIfMissing True (parent k)
     case v of
       Nothing -> pure ()
-      Just x  -> writeFileUtf8 (toFilePath k) x
+      Just x -> writeFileUtf8 (toFilePath k) x
+{-# INLINE runFSKVStoreAbsUtf8 #-}
